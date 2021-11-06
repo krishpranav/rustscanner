70d6d83e4ddce5f3d9aa8f3df83f1baec560d175
krishpranav <krisna.pranav@gmail.com>
2021-11-06 14:51:06 +0530

    src: async utils

--- src/async_utils.rs
+++ src/async_utils.rs
@@ -7,10 +7,11 @@ pub struct WaitGroup {
     task: Mutex<Cell<u64>>
 }
 
+/* wait group class */
 impl WaitGroup {
     pub async fn new() -> WaitGroup {
         WaitGroup {
-            task: Mutex::new(Cell::new(0))
+            task: Mutex::new(Cell::new(0)),
         }
     }
 
@@ -19,13 +20,20 @@ impl WaitGroup {
         task.set(task.get() + 1);
     }
 
-    pub asnyc fn done(&self) {
+    pub async fn done(&self) {
         let task = self.task.lock().await;
         task.set(task.get() - 1);
     }
 
     pub async fn wait(&self) {
-        let task = self.task.lock().await;
-        task.set(task.get() / 1);
+        loop {
+            let tk = self.task.lock().await;
+            if tk.get() == 0 {
+                break;
+            }
+
+            time::sleep(time::Duration::from_millis(100)).await;
+        }
     }
 }
+
