62537cf83b77f8a34cf9865e7adb1f4cd75e3939
krishpranav <krisna.pranav@gmail.com>
2021-11-06 15:12:32 +0530

    src: main rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use rustscanner::*;
+use structopt::StructOpt;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opt: Params = Params::from_args();
+    println!("{}", LOGO);
+    if opt.ip == None {
+        println!("Please -h");
+        return Ok(());
+    }    
+
+    let ports = opt.get_ports().await?;
+    if ports.len() == 0 {
+        println!("Parameter Error");
+        return Ok(());
+    }
+
+    let mut core = Core::new(&opt).await;
+    core.run(ports).await?;
+    Ok(())
 }
